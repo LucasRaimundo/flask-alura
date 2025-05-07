345113f79eb81f5092bc9676ea9f525daec07f82
Lucas Raimundo <lucasraimundo2010@gmail.com>
2025-05-07 12:13:13 -0300

    creating class game

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
 
+class Game:
+    def __init__(self, name, genre, platform):
+        self.name = name
+        self.genre = genre
+        self.platform = platform
+
 
 app = Flask(__name__)
 
 @app.route('/home')
 def home():
-    games = ['Fortinte', 'Hogwarts Legacy', 'Call of Duty', 'Fifa 23']
+    game1 = Game('Fortnite', 'Battle Royale', 'All')
+    game2 = Game('Hogwarts Legacy', 'Action RPG', 'All')
+    game3 = Game('Call of Duty', 'FPS', 'All')
+    games = [game1, game2, game3]
     return render_template('lista.html', titulo='Games', games=games)
 
 app.run()
