6d1068808f6e5c0d4e06ea3febdc294239ce7d31
Lucas Raimundo <lucasraimundo2010@gmail.com>
2025-05-07 13:00:56 -0300

    creating new game

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 class Game:
     def __init__(self, name, genre, platform):
@@ -6,15 +6,33 @@ class Game:
         self.genre = genre
         self.platform = platform
 
+game1 = Game('Fortnite', 'Battle Royale', 'All')
+game2 = Game('Hogwarts Legacy', 'Action RPG', 'All')
+game3 = Game('Call of Duty', 'FPS', 'All')
+games = [game1, game2, game3]
+
 
 app = Flask(__name__)
 
-@app.route('/home')
+@app.route('/')
 def home():
-    game1 = Game('Fortnite', 'Battle Royale', 'All')
-    game2 = Game('Hogwarts Legacy', 'Action RPG', 'All')
-    game3 = Game('Call of Duty', 'FPS', 'All')
-    games = [game1, game2, game3]
+    
     return render_template('lista.html', titulo='Games', games=games)
 
-app.run()
+@app.route('/newgame')
+def new_game():
+    return render_template('new.html', titulo='New Game')
+
+@app.route('/games/new', methods=['POST'])
+def create_game():
+    name = request.form['name']
+    genre = request.form['genre']
+    platform = request.form['platform']
+
+    game = Game(name, genre, platform)
+    games.append(game)
+    return render_template('lista.html', titulo='Games', games=games)
+
+
+
+app.run(debug=True)
